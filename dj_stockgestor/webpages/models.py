caf1a6ccc25d340f08db575be2336860432024a3
Erico <erico.chen@hotmail.com>
2024-03-26 14:25:57 -0300

    criei a base de dados fornecedores

--- dj_stockgestor/webpages/models.py
+++ dj_stockgestor/webpages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+#IE IM ENDERECO UF EMAIL TELEFONE
+class Fornecedores(models.Model):
+    nome_empresa = models.CharField(max_length = 40)
+    cnpj = models.CharField(max_length = 18)
+    inscricao_estadual = models.CharField(max_length = 15)
+    inscricao_municipal = models.CharField(max_length = 15)
+    fornecedor_email = models.EmailField(max_length = 40)
+    fornecedor_telefone = models.CharField(max_length = 12)
