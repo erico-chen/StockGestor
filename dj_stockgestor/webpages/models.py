9ba6b13c1221128ddd7aa9068245ade662a3ab58
Erico <erico.chen@hotmail.com>
2024-03-26 19:30:18 -0300

    Integrei o Formulário de fornecedores com a base de dados de Fornecedores

--- dj_stockgestor/webpages/models.py
+++ dj_stockgestor/webpages/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-#IE IM ENDERECO UF EMAIL TELEFONE
+
 class Fornecedores(models.Model):
     nome_empresa = models.CharField(max_length = 40)
     cnpj = models.CharField(max_length = 18)
     inscricao_estadual = models.CharField(max_length = 15)
     inscricao_municipal = models.CharField(max_length = 15)
+    endereco = models.CharField(max_length = 40, default="NULL")
+    uf = models.CharField(max_length = 2, default="NULL")
     fornecedor_email = models.EmailField(max_length = 40)
     fornecedor_telefone = models.CharField(max_length = 12)
--- dj_stockgestor/webpages/views.py
+++ dj_stockgestor/webpages/views.py
@@ -1,9 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Fornecedores
 
 
 def home(request):
-    return render(request, 'webpages/base.html')
+    return render(request, 'webpages/base_home.html')
 
 def logado(request):
-    return render(request, 'webpages/testando.html')
+    if request.method == "POST":
+        form = request.POST
+        fornecedor = Fornecedores(nome_empresa=form["nome_empresa"], cnpj=form["cnpj"], inscricao_estadual=form["ie"]\
+                                , inscricao_municipal=form["im"], endereco=form["endereco"], uf=form["uf"]\
+                                , fornecedor_email=form["email"], fornecedor_telefone=form["telefone"])
+        fornecedor.save()
+       
+
+    return render(request, 'webpages/base_logada.html')
 
